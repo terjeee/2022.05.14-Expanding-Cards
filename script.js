9d3952d677db9346456b79d9c91f8ae4d612a138
terjeee <96795863+terjeee@users.noreply.github.com>
2022-05-15 05:50:26 +0200

    ferdig

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ const panels = document.querySelectorAll(`.panel`);
 // ---- //addEventListener // ---- //
 
 panels.forEach(panel => {
-  panel.addEventListener(`click`, function (event) {
-    panels.forEach(panel => panel.classList.remove(`panel--active`));
+  panel.addEventListener(`click`, function () {
+    // decrease ALL panel sizes, hide ALL text
+    panels.forEach(panel => {
+      panel.classList.remove(`panel--active`);
+      panel.firstElementChild.classList.add(`hidden`);
+    });
+
+    // increase panel size, add panel text
     panel.classList.add(`panel--active`);
+    panel.firstElementChild.classList.remove(`hidden`);
   });
 });
